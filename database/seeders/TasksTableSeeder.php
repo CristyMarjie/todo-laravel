44b095d478a8cc8478509bfad5d46d69db4bb737
CristyMarjie <bragacristymarjie@gmail.com>
2023-04-09 00:19:09 +0800

    last class discussion before holiday

--- database/seeders/TasksTableSeeder.php
+++ database/seeders/TasksTableSeeder.php
@@ -14,5 +14,31 @@ class TasksTableSeeder extends Seeder
     public function run(): void
     {
         Task::factory()->count(2)->create();
+        $tasks = [
+            [
+                'avatar' => null,
+                'name' => 'Lean Jerios',
+                'description' => 'Teach Laravel',
+                'priority' => 'high',
+                'completed' => false
+            ],
+            [
+                'avatar' => null,
+                'name' => 'Prof. Winfield Koch DDS',
+                'description' => 'Teach Laravel',
+                'priority' => 'low',
+                'completed' => false
+            ],
+            [
+                'avatar' => null,
+                'name' => 'Kameron Yundt',
+                'description' => 'Teach Laravel',
+                'priority' => 'med',
+                'completed' => false
+            ]
+        ];
+        foreach ($tasks as $task){
+            Task::create($task);
+        }
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use App\Models\User;
 
 class UsersTableSeeder extends Seeder
@@ -14,5 +15,29 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(5)->create();
+        // $users = [
+        //     [
+        //         'name' => 'Cristy Marjie Braga',
+        //         'email' => 'cristy@gmail.com',
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        //     ],
+        //     [
+        //         'name' => 'David Smith',
+        //         'email' => 'david@gmail.com',
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        //     ]
+        // ];
+        // foreach ($users as $user){
+        //     User::create($user);
+        // }
+
+        //create a single static record
+        User::create([
+            'name' => 'Cristy Marjie Braga',
+            'email' => 'cristymarjie@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
